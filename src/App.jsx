b1bb813337c28bfd30f1ce957ef2b0e840d34340
Ha Chi HOANG <chibienayme@gmail.com>
2023-04-27 11:07:39 +0200

    To do App

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,103 @@
 import { useReducer, useRef, useState } from "react";
 
-// import Updown from "./components/Updown"
-
 //1. Init state: 0
 const initState = {
-  job: '',
-  jobs: [],
+  task: '',
+  todoList: []
 }
 
 //2. Actions
-const SET_JOB = 'set_job'
-const ADD_JOB = 'add_job'
-const DELETE_JOB = 'delete_job'
-const EDIT_JOB = 'edit_job'
-const DELETE_ALL_JOBS = 'delete_all_jobs'
+const SET_TASK = 'set_task'
+const ADD_TASK = 'add_task'
+const DEL_TASK = 'del_task'
+const EDIT_TASK = 'edit_task'
+const CLEAR_LIST = 'clear_list'
 
-//payload: dữ liệu mang theo đi (tương ứng với dữ liệu user gõ vào input) 
-const setJob = payload => {
+
+// convert actions to functions
+const setTask = payload => {
   return {
-      type: SET_JOB,
-      payload
+    type: SET_TASK,
+    payload
+  }
+}
+const addTask = payload => {
+  return {
+    type: ADD_TASK,
+    payload
+  }
+}
+const delTask = payload => {
+  return {
+    type: DEL_TASK,
+    payload
+  }
+}
+const editTask = payload => {
+  return {
+    type: EDIT_TASK,
+    payload
+  }
+}
+const clearList = payload => {
+  return {
+    type: CLEAR_LIST,
+    payload
   }
 }
 
-const addJob = payload => {
-  return {
-      type: ADD_JOB,
-      payload
-  }
-}
 
-const deleteJob = payload => {
-  return {
-      type: DELETE_JOB,
-      payload
-  }
-}
-
-const editJob = payload => {
-  return {
-      type: EDIT_JOB,
-      payload
-  }
-}
-
-const deleteAllJobs = payload => {
-  return {
-      type: DELETE_ALL_JOBS,
-      payload
-  }
-}
-
-//3. Reducer
+//3. Reducer (switch cases)
 const reducer = (state, action) => {
   console.log('Action: ', action)
   console.log('Prev State: ', state)
 
-  let newState 
+  let newState;
 
-  switch(action.type) {
-    case SET_JOB:
+  switch (action.type) {
+    case SET_TASK:
       newState = {
-        ...state,
-        job: action.payload
+        ...state, 
+        task: action.payload
       }
       break
 
-    case ADD_JOB:
+    case ADD_TASK:
       newState = {
         ...state,
-        jobs: [...state.jobs, action.payload]
+        todoList: [...state.todoList, action.payload]
       }
       break
 
-    case DELETE_JOB: {
-      const newJobs = [...state.jobs]
-      newJobs.splice(action.payload, 1)
+    case DEL_TASK: {
+      const newTodoList = [...state.todoList]
+      newTodoList.splice(action.payload, 1)
       newState = {
         ...state,
-        jobs: newJobs
+        todoList: newTodoList
       }
     }
-    break
+      break
 
-    case EDIT_JOB: {
-      const updatedJob = [...state.jobs]
-      updatedJob[action.payload.index] = action.payload.value
+    case EDIT_TASK: {
+      const updatedTask = [...state.todoList]
+      updatedTask[action.payload.index] = action.payload.value
       newState = {
         ...state,
-        jobs: updatedJob
+        todoList: updatedTask
       }
     }
-    break
+      break
 
-    case DELETE_ALL_JOBS:
+    case CLEAR_LIST:
       newState = {
         ...state,
-        jobs: []
+        todoList: []
       }
-    break
+      break
 
-    default: 
-      throw new Error('Invalid action')
+    default:
+      throw new Error ("Invalid action type")
   }
 
   console.log('New State: ', newState)
@@ -113,10 +109,8 @@ const reducer = (state, action) => {
 
 
 function App() {
-  
   const [state, dispatch] = useReducer(reducer, initState)
-
-  const {job, jobs} = state
+  const {task, todoList} = state
 
   const [editIndex, setEditIndex] = useState(null)
   const [editValue, setEditValue] = useState('')
@@ -124,18 +118,21 @@ function App() {
   const inputRef = useRef()
 
   const handleSubmit = () => {
-    dispatch(addJob(job))
-    dispatch(setJob('')) //return empty input
-
-    inputRef.current.focus() //focus again into input
+    if(task !== '') { 
+      dispatch(addTask(task))
+      dispatch(setTask(''))
+  
+      inputRef.current.focus()
+    }
   }
-
+  
   const handleEdit = (index, value) => {
-    dispatch(editJob({index, value}))
+    dispatch(editTask({index, value}))
     setEditIndex(null)
     setEditValue('')
   }
-
+  
+  
   return (
     <div style={{ padding: 50 }}>
 
@@ -143,41 +140,41 @@ function App() {
 
       <input 
           ref={inputRef}
-          value={job}
+          value={task}
           placeholder="Enter a task..." 
           onChange={e => {
-              dispatch(setJob(e.target.value))
+            dispatch(setTask(e.target.value))
           }}
-          onKeyUp={e => e.key === "Enter" && handleSubmit()}
+          onKeyUp={e => e.code === "Enter" && handleSubmit()}
       />
-      <button onClick={handleSubmit}>Add</button>
+
+      <button onClick={handleSubmit}> Add </button>
       
       <ul>
-        {jobs.map((job, index) => (
+        {todoList.map((task, index) => (
           <li key={index}>
             {editIndex === index ? (
               <>
-                <input
+                <input 
                   type="text"
-                  defaultValue={job}
-                  onChange={(e) => {
-                    setEditValue(e.target.value);
-                  }}
+                  defaultValue={task}
+                  onChange={e => {
+                    setEditValue(e.target.value)
+                  }} 
                 />
 
                 <button 
                   onClick={() => handleEdit(index, editValue)}
                 >
-                  Done
+                  Save
                 </button>
               </>
             ) : (
               <>
-                {job}
+                {task}
 
-                <button
-                  onClick={() => {
-                    dispatch(deleteJob(index));
+                <button onClick={() => {
+                    dispatch(delTask(index))
                   }}
                 >
                   X
@@ -185,21 +182,25 @@ function App() {
 
                 <button 
                   onClick={() => setEditIndex(index)}
-                >
-                  Edit
+                > 
+                  Edit 
                 </button>
+
               </>
-
             )}
-
           </li>
         ))}
-
+         
       </ul>
 
-      {jobs.length > 1 && (
-        <button onClick={() => dispatch(deleteAllJobs())}> Clear </button>
-      )}
+        {todoList.length >= 1 && (
+          <button 
+            onClick={() => dispatch(clearList())}
+          >
+            Clear all
+          </button>
+        )}
+      
     </div>
   )
 }
